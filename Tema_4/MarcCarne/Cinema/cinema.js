e6ec7cca230772ca0f57ceaa7bc409036b615cdc
Marc Carne <mcarne@innova.local>
2023-11-22 08:31:31 +0100

    Ajuste en el JS de cinema para aprovechar el bucle para añadir el id a los botones y la creación de la matrix

--- Tema_4/MarcCarne/Cinema/cinema.js
+++ Tema_4/MarcCarne/Cinema/cinema.js
@@ -1,39 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Inicializar la matriz de butacas
+    // Dado que la matrix del JS es la misma que la del HTML en vez de crearla
+    // a partir del número de filas y columnas la creamos leyendo los elementos
+    // del HTML
     let seats = createSeatMatrix();
     seats[7][7].setStatus(true);
-
-    // Adquirir la parte del HTML en la que se definen la matrix de botones
-    let seatsMatrix = document.getElementById("seats");
-    // Recorrer todas las filas y todos los botones para modificar el ID.
-    // El boton superior izquierda de la matriz es el id 1 y el boton inferior derecho
-    // el 64 (ya que es una matriz 8*8)
-    let idx = 1;
-    seatsMatrix.querySelectorAll("div").forEach(row => {
-        row.querySelectorAll("button").forEach(button => {
-            button.id = "button" + idx.toString();
-            idx++;
-        })
-    });
 
     // Añadir el listener a input
     let seatsNumElem = document.getElementById("seats-num")
     seatsNumElem.addEventListener("input", () => {
         // Eliminar la clase que muestra la selección a todos los asientos
         document.getElementById("seats").querySelectorAll("button").forEach(button => {
-            button.classList.remove("seat-candidate")
+            button.classList.remove("seat-candidate");
         })
+        // Obtener propuesta de asientos y añadirle la clase que le modifica el color al botón
         let seatsIds = suggest(seatsNumElem.value, seats)
         seatsIds.forEach(seatId => {
-            document.getElementById("button" + seatId.toString()).classList.toggle("seat-candidate")
+            document.getElementById("button" + seatId.toString()).classList.add("seat-candidate");
         })
     });
 
 });
-
-// Definición de la matriz
-const numFilas = 8;
-const numAsientosFila = 8;
 
 // Clase seat, para definir las propiedades de un asiento
 class Seat {
@@ -57,19 +44,26 @@ class Seat {
 
 // Función para generar la matriz
 function createSeatMatrix() {
-    let idButaca = 1;
+    let idSeat = 1;
     let seats = []; // Array vacío
 
-    for (let fila = 0; fila < numFilas; fila ++) {
+    // Adquirir la parte del HTML en la que se definen la matrix de botones
+    let seatsMatrix = document.getElementById("seats");
+    // Recorrer todas las filas y todos los botones para modificar el ID.
+    // El boton superior izquierda de la matriz es el id 1 y el boton inferior derecho
+    // el 64 (ya que es una matriz 8*8)
+    // A la vez vamos generando la matrix de asientos del JS
+    seatsMatrix.querySelectorAll("div").forEach(row => {
         let fila = []; // Array de la fila
-        for (let asiento = 0; asiento < numAsientosFila; asiento++) {
+        row.querySelectorAll("button").forEach(button => {
+            button.id = "button" + idSeat.toString();
             fila.push(
-                new Seat(idButaca)
+                new Seat(idSeat)
             );
-            idButaca++;
-        }
+            idSeat++;
+        });
         seats.push(fila);
-    }
+    });
     return seats;
 }
 
@@ -79,7 +73,7 @@ function findSeatsInFila(numAsientos, filaArray) {
     let seatCount = 0; // Control de los asientos asignados
     // Búsqueda desde la izquierda
     let seat = 0;
-    while ((seatCount < numAsientos) && (seat < numAsientosFila)) {
+    while ((seatCount < numAsientos) && (seat < filaArray.length)) {
         if (filaArray[seat].isAvailable()) {
             seatCount++;
             suggestedSeats.add(filaArray[seat].getId());
@@ -100,12 +94,13 @@ function findSeatsInFila(numAsientos, filaArray) {
 // contiguos
 function suggest(numAsientos = 0, seatsArray) {
     let suggestedSeats = new Set();
+    let matrixSize = seatsArray.length;
     // Comprobación de tamaño de fila
-    if (numAsientos > numAsientosFila) {
+    if (numAsientos > matrixSize) {
         return suggestedSeats;
     } else {
         // Busqueda desde la última fila
-        for (let fila = numFilas - 1; fila >= 0 && suggestedSeats.size == 0; fila--) {
+        for (let fila = matrixSize - 1; fila >= 0 && suggestedSeats.size == 0; fila--) {
             suggestedSeats = findSeatsInFila(numAsientos, seatsArray[fila]);
         }
     }
