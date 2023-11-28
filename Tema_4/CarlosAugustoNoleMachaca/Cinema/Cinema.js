d1ee99507c35321f91d257163049c49a59cdf942
carnol1989 <carnol1989@hotmail.com>
2023-11-28 00:47:06 -0500

    feat: solution topic 4

--- Tema_4/CarlosAugustoNoleMachaca/Cinema/Cinema.js
+++ Tema_4/CarlosAugustoNoleMachaca/Cinema/Cinema.js
@@ -1,5 +1,67 @@
 // Definir el tamaño de la matriz de butacas
 const N = 10; // Número de filas y columnas
+const LETTERS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+
+let butacas = [];
+let seatsReserved = new Set();
+function initialize() {
+    butacas = setup();
+
+    let divZonaButacas = "";
+
+    let butaca = "";
+    let divRow = "";
+    let divRowLast = "";
+    let divCol = "";
+    let divColLast = "";
+    for (let i = 0; i < butacas.length; i++) {
+        divRow = `<div class="row">`;
+        divCol = `<div class="col">
+                            ${LETTERS[i]}
+                         </div>
+                        <div class="col"></div>`;
+
+        if (i === butacas.length - 1) {
+            divRowLast = `<div class="row">
+                            <div class="col">
+                            </div>
+                            <div class="col">
+                    
+                            </div>`
+        }
+        for (let j = 0; j < butacas[i].length; j++) {
+            if (i === 0 && j === 3) {
+                divCol += `<div id="${butacas[i][j].id}" class="col-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-wheelchair" viewBox="0 0 16 16">
+                                <path d="M12 3a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Zm-.663 2.146a1.5 1.5 0 0 0-.47-2.115l-2.5-1.508a1.5 1.5 0 0 0-1.676.086l-2.329 1.75a.866.866 0 0 0 1.051 1.375L7.361 3.37l.922.71-2.038 2.445A4.732 4.732 0 0 0 2.628 7.67l1.064 1.065a3.25 3.25 0 0 1 4.574 4.574l1.064 1.063a4.732 4.732 0 0 0 1.09-3.998l1.043-.292-.187 2.991a.872.872 0 1 0 1.741.098l.206-4.121A1 1 0 0 0 12.224 8h-2.79l1.903-2.854ZM3.023 9.48a3.25 3.25 0 0 0 4.496 4.496l1.077 1.077a4.75 4.75 0 0 1-6.65-6.65l1.077 1.078Z"/>
+                            </svg>
+                          </div>`;
+                j = 6;
+            } else {
+                divCol += `<div class="col">
+                            <div id="${butacas[i][j].id}" class="dot-available"></div>
+                          </div>`;
+            }
+
+            if (i === butacas.length - 1) {
+                divColLast += `<div class="col">
+                                ${j + 1}
+                            </div>`;
+            }
+        }
+        divRow += divCol;
+        divRow += `</div>`;
+        butaca += divRow;
+
+        divRowLast += divColLast;
+        divRowLast += "</div>";
+    }
+
+    divZonaButacas = butaca + divRowLast;
+
+    let zonaButacas = document.getElementById("zonaButacas");
+    zonaButacas.innerHTML = divZonaButacas;
+}
 
 // Función para inicializar la matriz de butacas
 function setup() {
@@ -22,10 +84,10 @@ function setup() {
 }
 
 function suggest(nroSeatsReserve) {
-    let butacas = setup();
-
     let band = 0;
-    const seatsReserved = new Set();
+
+    butacas = setup();
+    seatsReserved = new Set();
 
     if (nroSeatsReserve > butacas.length) {
         console.log("Excede al número de filas.");
@@ -47,9 +109,7 @@ function suggest(nroSeatsReserve) {
         }
     }
 
-    seatsReserved.forEach (function(value) {
-        console.log(value);
-    });
+    avaibleSeats();
 }
 
 function validateNumberOfAvaibleRows(i, butacas, nroSeatsReserve){
@@ -68,45 +128,44 @@ for (let j = butacas[i].length - 1; j >= 0 && band < nroSeatsReserve; j--) {
 return result;
 }
 
-function printButacas(butacas) {
-for (let i = 0; i < butacas.length; i++) {
-    for (let j = i; j < butacas[i]; j++) {
-        printButaca(i, j, butacas);
+function preselectSeats(seatReserved) {
+    //console.log("preselectSeats");
+    let preselectSeatReserved = document.getElementById(seatReserved);
+    preselectSeatReserved.classList.remove("dot-available");
+    preselectSeatReserved.classList.add("dot-active");
+}
+
+function avaibleSeats() {
+    //console.log("avaibleSeats");
+    for (let i = butacas.length - 1; i >= 0; i--) {
+        for (let j = butacas[i].length - 1; j >= 0; j--) {
+            if (butacas[i][j].id !== undefined && butacas[i][j].id !== null) {
+                let preselectSeatReserved = document.getElementById(butacas[i][j].id);
+                if (preselectSeatReserved !== null && preselectSeatReserved !== undefined) {
+                    if (isSeatReserved(butacas[i][j].id)) {
+                        if(preselectSeatReserved.classList.contains("dot-available")) {
+                            preselectSeatReserved.classList.remove("dot-available");
+                        }
+                        preselectSeatReserved.classList.add("dot-active");
+                    } else {
+                        if(preselectSeatReserved.classList.contains("dot-active")) {
+                            preselectSeatReserved.classList.remove("dot-active");
+                        }
+                        preselectSeatReserved.classList.add("dot-available");
+                    }
+                }
+            }
+        }
     }
 }
-}
 
-function printButaca(i, j, butacas) {
-console.log("butacas [" + i + "][" + j + "]: " + butacas[i][j]);
-console.log("id: " + butacas[i][j].id);
-console.log("estado: " + butacas[i][j].estado);
-}
-
-function testCase1(butacas) {
-butacas[9][6].estado = true;
-butacas[9][7].estado = true;
-butacas[9][8].estado = true;
-butacas[9][9].estado = true;
-}
-
-function testCase2(butacas) {
-testCase1(butacas);
-butacas[9][5].estado = true;
-butacas[9][4].estado = true;
-butacas[9][3].estado = true;
-butacas[9][2].estado = true;
-butacas[9][1].estado = true;
-butacas[9][0].estado = true;
-butacas[8][6].estado = true;
-butacas[8][5].estado = true;
-butacas[8][4].estado = true;
-butacas[8][3].estado = true;
-butacas[8][2].estado = true;
-}
-
-function testCase3(butacas) {
-testCase2(butacas);
-butacas[8][9].estado = true;
-butacas[8][8].estado = true;
-butacas[8][7].estado = true;
+function isSeatReserved(idFind) {
+    let result = false;
+    for (let item of seatsReserved) {
+        if (item === idFind) {
+            result = true;
+            break;
+        }
+    }
+    return result;
 }
--- Tema_4/CarlosAugustoNoleMachaca/Cinema/formRegistro.js
+++ Tema_4/CarlosAugustoNoleMachaca/Cinema/formRegistro.js
@@ -95,7 +95,8 @@ document.getElementById('userForm').addEventListener('submit', (event) => {
     let errorMessages = document.querySelectorAll('form p');
     if (errorMessages.length === 0) {
         // No hay errores, se puede procesar el formulario
-        alert('Formulario enviado con éxito!');
+        //alert('Formulario enviado con éxito!');
+        window.location.href = "Cinema.html";
     } else {
         // Hay errores, se informa al usuario
         alert('Por favor, corrija los errores antes de enviar el formulario.');
