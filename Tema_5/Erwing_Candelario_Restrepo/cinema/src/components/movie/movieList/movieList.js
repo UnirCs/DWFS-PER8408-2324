93cdd628c0927d93d8caf40d9184cebc075fc7e7
Erwing Candelario <ingeniero_79@hotmail.com>
2023-12-03 22:35:43 -0500

    Tema 5

---
+++ Tema_5/Erwing_Candelario_Restrepo/cinema/src/components/movie/movieList/movieList.js
@@ -0,0 +1,61 @@
+export const MovieList = [
+	{
+		titulo: 'Los mercenarios',
+		sinopsis: 'El veterano mercenario Barney “Esquizo” Ross, deberá afrontar un nuevo desafío',
+		duracion: '152',
+		imagen:
+			'https://hips.hearstapps.com/hmg-prod/images/los-mercenarios-sylvester-stallone-jason-statham-1633803063.jpg?crop=1.00xw:0.519xh;0,0&resize=980:*',
+		genero: 'Acción',
+		puntuacion: 9.5,
+	},
+	{
+		titulo: 'El Exorcista: Creyente',
+		sinopsis:
+			'Cuando su hija Angela y su amiga Katherine muestran signos de posesión demoníaca, se desata una cadena de acontecimientos.',
+		duracion: '175',
+		imagen:
+			'https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/cinefilia/david-fincher-peliculas-favoritas/1973.-el-exorcista/138075726-1-esl-ES/1973.-EL-EXORCISTA.jpg?crop=1xw:1xh;center,top&resize=980:*',
+		genero: 'Terror',
+		puntuacion: 9.6,
+	},
+	{
+		titulo: 'Origen',
+		sinopsis:
+			'Dom Cobb (Leonardo DiCaprio) es un ladrón con la rara habilidad de entrar en los sueños de las personas y robar sus secretos de su subconsciente.',
+		duracion: '148',
+		imagen:
+			'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRahMdIAUM-EOFYqEUdg55XHG4JQYaskXKhDG8WeHkyZH2DXZVz',
+		genero: 'Ciencia ficción',
+		puntuacion: 7.5,
+	},
+	{
+		titulo: 'Spider-Man: Cruzando el multiverso',
+		sinopsis:
+			'La versión de Miles Morales de Spidey, atraviesa multiversos y se encuentra con cientos de Spiderman paralelos en una búsqueda personal con intereses universales.',
+		duracion: '142',
+		imagen:
+			'https://hips.hearstapps.com/hmg-prod/images/spider-man-into-the-spider-verse-1667898373.jpg?crop=1xw:1xh;center,top&resize=980:*',
+		genero: 'Infantil',
+		puntuacion: 9.0,
+	},
+	{
+		titulo: 'The Marvels',
+		sinopsis:
+			'La Capitana Marvel, recuperó su identidad del tiránico Kree y se vengó de la Inteligencia Suprema..',
+		duracion: '169',
+		imagen:
+			'https://play-lh.googleusercontent.com/9q_v3tzC0NTU9zwbda3uWHo0uctZVnMbgQaCCFzWGvcoXEphqWqXGvWQlZ-3yIJXuOoU',
+		genero: 'Ciencia ficción',
+		puntuacion: 7,
+	},
+	{
+		titulo: 'Blue Beetle',
+		sinopsis:
+			'Reyes es un estudiante de instituto que descubre que tiene superpoderes y debe utilizarlos para luchar contra el crimen.',
+		duracion: '154',
+		imagen:
+			'https://hips.hearstapps.com/hmg-prod/images/blue-beetle-1665057115.jpg?crop=1xw:1xh;center,top&resize=980:*',
+		genero: 'Ciencia ficción',
+		puntuacion: 8.0,
+	},
+];
