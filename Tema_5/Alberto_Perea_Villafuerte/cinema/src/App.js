425443422bcb22ef93becb559a4b23930a749264
alberto.perea <alberto.perea@msp.gob.ec>
2023-12-19 18:22:05 -0500

    Tema 5 primera parte read

--- Tema_5/Alberto_Perea_Villafuerte/cinema/src/App.js
+++ Tema_5/Alberto_Perea_Villafuerte/cinema/src/App.js
@@ -1,13 +1,1 @@
 import './App.css';
-import {Header} from "./components/Header";
-
-function App() {
-  return (
-    <div className="App">
-      <Header className="App-header">
-      </Header>
-    </div>
-  );
-}
-
-export default App;
--- Tema_5/Alberto_Perea_Villafuerte/cinema/src/components/Header.js
+++ Tema_5/Alberto_Perea_Villafuerte/cinema/src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Header = () => {
     return (
-      <header className="header-footer">
+      <header className="header-footer center-text">
             <h1 className="header-footer-text">UNIR-Cinema-Alberto-Perea</h1>
         </header>
     )
