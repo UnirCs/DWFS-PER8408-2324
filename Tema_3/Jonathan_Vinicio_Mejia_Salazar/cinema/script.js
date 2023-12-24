10ab0aca23008ac2ffe9b7f6ef8228f379023f1c
Jonathan Mejía <jmejia@montran.com>
2023-12-24 18:32:45 -0500

    Task 3

---
+++ Tema_3/Jonathan_Vinicio_Mejia_Salazar/cinema/script.js
@@ -0,0 +1,74 @@
+// Definir el tamaño de la matriz de butacas
+const N = 10; // Número de filas y columnas
+
+// Función para inicializar la matriz de butacas
+function setup() {
+    let idContador = 1; // Iniciar el contador de IDs en 1 (los humanos no empezamos a contar desde 0)
+    let butacas = [];
+
+    for (let i = 0; i < N; i++) {
+        // Nueva fila
+        let fila = [];
+        for (let j = 0; j < N; j++) {
+            // Nuevo asiento
+            fila.push({
+                id: idContador++,
+                estado: false // Estado inicial libre
+            });
+        }
+        butacas.push(fila);
+    }
+    return butacas;
+}
+
+// Inicializar la matriz
+let butacas = setup();
+
+
+function suggest(nAsientos){
+
+    let filas = butacas.length;
+
+    if(nAsientos>butacas.length){
+        return [];
+    }
+
+    for (let i = filas-1; i >= 0; i--) {
+
+        for (let j = butacas[i].length-1; j >= 0; j--) {
+            if((j+1-nAsientos)<0){
+                break;
+            }
+            let aux = 0;
+            let aux2 = j;
+            let thereIsButacas = true;
+            while(aux<=nAsientos){
+                aux++;
+                if(!butacas[i][aux2].estado && aux == nAsientos){
+                    thereIsButacas = true;
+                    break;
+                } else if(butacas[i][aux2].estado){
+                    thereIsButacas=false;
+                    break;
+                }
+                aux2--;
+            }
+
+            if(thereIsButacas){
+                let result = [];
+                let x = j;
+                for (let z = 1; z <= nAsientos; z++) {
+                    butacas[i][x].estado=true;
+                    result.push(butacas[i][x].id)
+                    x--;
+                }
+                console.log(result);
+                return result;
+            }
+
+        }
+    }
+    return [];
+
+}
+
