73d0816bcc973eedd9e01ded6dc89db7d43a238e
vicenteeduardo-rodriguez043 <151455266+vicenteeduardo-rodriguez043@users.noreply.github.com>
2024-02-07 18:53:59 -0500

    Segunda entrega (#254)
    * primera entrega
    
    * primer
    
    * deber desde la tarea2 hasta la tarea5
    
    ---------
    
    Co-authored-by: vicenteeduardo-rodriguez043 <vicenteeduardo-rodriguez043@comunidadunir.net>

---
+++ Tema_2/Vicente_Rodriguez_Echeverria/cinema/cinema.js
@@ -0,0 +1,61 @@
+// Definir el tamaño de la matriz de butacas
+const N = 10; // Número de filas y columnas
+
+// Función para inicializar la matriz de butacas
+function setup() {
+    let idContador = 1; // Iniciar el contador de IDs en 1 (los humanos no empezamos a contar desde 0)
+    let butacas = [];
+
+    for (let i = 0; i < N; i++) {
+        // Nueva fila
+        let fila = [];
+        for (let j = 0; j < N; j++) {
+            // Nuevo asiento
+            fila.push({
+                id: idContador++,
+                estado: Math.random() < 0.5 // Estado inicial libre
+            });
+        }
+        butacas.push(fila);
+    }
+    return butacas;
+}
+
+function suggest(numeroAsiento, butacas){
+    let asientoReservado=new Set([]);
+    let completado=false;
+    let filaButaca,contadorReserva;
+    if(numeroAsiento>N) return asientoReservado;
+    for(let butaca=(N-1);butaca>=0;butaca--){
+        filaButaca=butacas[butaca];
+        //asientoReservado.clear();
+        for(let fila=0;fila<N;fila++){
+            contadorReserva=0;
+            for(let i=0;i<numeroAsiento;i++){
+                if((fila+numeroAsiento)<N){
+                    if(filaButaca[fila+i].estado===false){
+                        asientoReservado.add(filaButaca[fila+i].id);
+                        contadorReserva++;
+                    }
+                    else{
+                        asientoReservado.clear();
+                        i=numeroAsiento;
+                    }
+                }
+            }
+            if(contadorReserva===numeroAsiento){
+                butaca=-1;
+                fila=N;
+            }
+        }
+    }
+    return asientoReservado;
+}
+// Inicializar la matriz
+let butacas = setup();
+//imprime butaca
+console.log(butacas);
+//Inicializa reserva
+let reservado=suggest(5,butacas);
+// Imprimir la reserva
+console.log(reservado);
